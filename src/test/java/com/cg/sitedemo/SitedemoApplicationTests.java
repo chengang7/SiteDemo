40f3820486a66cfc4c7c4081aef27139cc46d91b
chengang7 <819240821@qq.com>
2018-12-04 14:31:39 +0800

    first commit

---
+++ src/test/java/com/cg/sitedemo/SitedemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cg.sitedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SitedemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
